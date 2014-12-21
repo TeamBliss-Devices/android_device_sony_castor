012f25f3a0b8994840f09ad3a851ea294ae536d8
Patrick Graber <pjgraber03@gmail.com>
2014-12-21 10:29:12 -0800

    blissified

--- bliss.mk
+++ bliss.mk
@@ -1,8 +1,8 @@
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/castor/full_castor.mk)
 
@@ -10,5 +10,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP521
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/SGP521/SGP521:4.4.4/23.0.1.A.0.167/WP_3Rw:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="SGP521-user 4.4.4 23.0.1.A.0.167 WP_3Rw release-keys"
 
-PRODUCT_NAME := cm_castor
+PRODUCT_NAME := bliss_castor
 PRODUCT_DEVICE := castor
